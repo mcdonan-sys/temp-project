49f4685cc1c110188970f38f2d076c733f30e8b9
mcdonan-sys <mcdonan@grace.edu>
2026-01-21 21:32:44 -0500

    Create Sock model for database representation
    This model represents a sock and includes a TODO for future validations.

---
+++ app/models/sock.rb
@@ -0,0 +1,10 @@
+# This model represents a sock in the database.
+# It maps to the socks table created by the migration.
+#
+# I'm learning that models handle data and database logic.
+#
+# TODO:
+# - Add validations (name, color required)
+# - Possibly restrict gender values
+class Sock < ApplicationRecord
+end
